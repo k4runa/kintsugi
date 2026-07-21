791125632dc5ddf3b70b1ed15aff3f12885a222d
k4runa <3nesslbr@gmail.com>
2026-07-21 18:39:55 +0300

    feat: update disk manager to use windows api

--- include/disk_manager.h
+++ include/disk_manager.h
@@ -6,6 +6,11 @@
 #include <fstream>
 #include <string>
 
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h> 
+#endif // _WIN32
+
 namespace Kintsugi::Storage 
 {
      // Disk manager class
@@ -14,7 +19,7 @@ namespace Kintsugi::Storage
           public:
                static constexpr std::size_t PAGE_SIZE = 4096;
                DiskManager(const std::string& db_file);
-               ~DiskManager() = default;
+               ~DiskManager();
 
                // functions
                void read_page(int page_id, char* page_data);
@@ -23,7 +28,11 @@ namespace Kintsugi::Storage
                int allocate_page();
 
           private:
+#ifdef _WIN32
+               HANDLE _hFile = INVALID_HANDLE_VALUE;
+#else 
                std::fstream _db_io;
+#endif
                std::string _file_name;
                int _next_page_id {};
      };
--- src/disk_manager.cpp
+++ src/disk_manager.cpp
@@ -4,12 +4,36 @@
 #include <iostream>
 #include <stdexcept>
 #include <string>
+#include <fcntl.h>
+
+#ifdef _WIN32
+#include <io.h>
+#endif
 
 namespace Kintsugi::Storage 
 {
      DiskManager::DiskManager(const std::string& db_file) : _file_name(db_file)
      {
-          // open the file, if fail or not exists, create a new one.
+          // open the file, if fail or not exists, create a new one
+#ifdef _WIN32
+          HANDLE hFile = CreateFile(
+               _file_name.c_str(),
+               GENERIC_READ | GENERIC_WRITE,
+               FILE_SHARE_READ | FILE_SHARE_WRITE,
+               NULL,
+               OPEN_ALWAYS,
+               FILE_ATTRIBUTE_NORMAL,
+               NULL
+          );
+
+          if(hFile == INVALID_HANDLE_VALUE)
+          {
+               throw std::runtime_error("Could not open file: " + _file_name);
+          }
+
+          std::cout << "INFO: Database created successfully: " << _file_name << std::endl;
+
+#else
           _db_io.open(_file_name, std::ios::in | std::ios::out | std::ios::binary);
      
           //check if file couldn't open, or not exists
@@ -32,10 +56,21 @@ namespace Kintsugi::Storage
 
                std::cout << "INFO: Database created successfully: " << _file_name << std::endl;
           }
+#endif //_WIN32
      }
 
      void DiskManager::read_page(int page_id, char* page_data)
      {
+#ifdef _WIN32
+          OVERLAPPED overlapped = {0};
+          overlapped.Offset = page_id * PAGE_SIZE;
+          DWORD bytesRead;
+
+          if(!ReadFile(_hFile, page_data, PAGE_SIZE, &bytesRead, &overlapped))
+          {
+               throw std::runtime_error("Could not read page, page id: " + std::to_string(page_id));
+          }
+#else
           _db_io.seekg(static_cast<std::streamoff>(page_id) * PAGE_SIZE);
           _db_io.read(page_data, PAGE_SIZE);
 
@@ -43,10 +78,24 @@ namespace Kintsugi::Storage
           {
                throw std::runtime_error("Could not read page, page id: " + std::to_string(page_id));
           }
+
+#endif // _WIN32
      }
 
      void DiskManager::write_page(int page_id, const char* page_data)
      {
+#ifdef _WIN32
+          OVERLAPPED overlapped = {0};
+          overlapped.Offset = page_id * PAGE_SIZE;
+          DWORD bytesWritten;
+
+          if(!WriteFile(_hFile, page_data, PAGE_SIZE, &bytesWritten, &overlapped))
+          {
+               throw std::runtime_error("Could not write to page, page id: " + std::to_string(page_id));
+          }
+
+          FlushFileBuffers(_hFile);
+#else
           _db_io.seekg(static_cast<std::streamoff>(page_id) * PAGE_SIZE);
           _db_io.write(page_data, PAGE_SIZE);
 
@@ -56,6 +105,22 @@ namespace Kintsugi::Storage
           }
 
           _db_io.flush();
+#endif // _WIN32
+     }
+
+     DiskManager::~DiskManager()
+     {
+#ifdef _WIN32
+          if(_hFile != INVALID_HANDLE_VALUE)
+          {
+               CloseHandle(_hFile);
+          }
+#else
+          if(_db_io.is_open())
+          {
+               _db_io.close();
+          }
+#endif
      }
 
      int DiskManager::allocate_page() { return _next_page_id++; }
